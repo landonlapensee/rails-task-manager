fff31e1afaea8100eaf68c6f12a1a281ac63dbc8
Landon Lapensee <landonlapensee@gmail.com>
2021-02-16 12:39:24 -0800

    Added Add/Create method to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new 
+    @task = Task.new
+  end 
+
+  def create
+    @new_task = Task.new(task_params)
+    @new_task.save
+
+    redirect_to task_path(@new_task)
+  end 
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :completed, :details)
+  end
 end
